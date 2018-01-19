a63043af81a714a90243b39ad3a89ad435281bb0
Simon Zhou <simon.zhou@ucla.edu>
2018-01-19 12:51:20 -0800

    First commit

---
+++ Assets/OnCollisionBling.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent( typeof( AudioSource ) )]
+public class OnCollisionBling : MonoBehaviour {
+	AudioSource audio;
+	void Start() {
+		audio = GetComponent<AudioSource>();
+	}
+	void OnCollisionEnter(Collision collision) {
+		if (collision.relativeVelocity.magnitude > 1) {
+			audio.pitch = Random.Range (0.8f, 1.2f);
+		}
+		audio.Play();
+	}
+} 
---
+++ Assets/PressToInstantantiateObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Valve.VR.InteractionSystem {
+	[RequireComponent( typeof( Interactable ) )]
+	public class PressToInstantantiateObject : MonoBehaviour {
+		float timer = 0;
+		float waitTime = 0.3f;
+		public GameObject objectToInstantiate;
+		void OnHandHoverBegin( Hand hand )    {
+			this.GetComponent<Renderer> ().material.SetColor("_Color", Color.green);
+		}
+		void HandHoverUpdate( Hand hand )    {
+			//Trigger got pressed
+			if ( hand.GetStandardInteractionButton() )
+			{    
+				this.GetComponent<Renderer> ().material.SetColor("_Color", Color.red);
+				if (timer >= waitTime) {
+					Instantiate (objectToInstantiate, this.transform.position + Vector3.up, Quaternion.identity);
+					timer = 0;
+				}
+			}
+			timer += Time.deltaTime;
+		}
+		void OnHandHoverEnd( Hand hand )    {
+			this.GetComponent<Renderer> ().material.SetColor("_Color", Color.blue);
+		}
+	}
+} 
