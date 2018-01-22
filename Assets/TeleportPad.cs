1fe6a8317e9bb478b0ec7f033962cf91d6e4e347
Simon Zhou <simon.zhou@ucla.edu>
2018-01-22 12:55:11 -0800

    Major update

---
+++ Assets/TeleportPad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPad : MonoBehaviour {
+	public int code;
+	float disableTimer = 0;
+
+
+	void Update() {
+		if (disableTimer > 0) {
+			disableTimer -= Time.deltaTime;
+		}
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.name == "Player" && disableTimer <= 0) {
+			foreach (TeleportPad tp in FindObjectsOfType<TeleportPad>()) {
+				if (tp.code == code && tp != this) {
+					tp.disableTimer = 2;
+					Vector3 position = tp.gameObject.transform.position;
+					position.y += 2;
+					collider.gameObject.transform.position = position;
+				}
+			}
+		}
+	}
+}
